6274fac474b1330d7c1abfe95cd573f0a46a6e6c
Julissa <Julissa@JULS>
2024-07-31 17:28:38 -0600

    Agregar archivos de proyecto.

---
+++ Calculardistancia5061893/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculardistancia5061893
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
---
+++ Calculardistancia5061893/ViewsModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculardistancia5061893.ViewsModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double _Varx1;
+        [ObservableProperty]
+        private double _Vary1;
+        [ObservableProperty]
+        private double _Varx2;
+        [ObservableProperty]
+        private double _Vary2;
+        [ObservableProperty]
+        private string? _distancia;
+
+
+        [RelayCommand]
+
+        private void CalcularDistancia()
+        {
+            var distancia = Math.Sqrt(
+                Math.Pow(Varx2 - Varx1, 2) +
+                Math.Pow(Vary2 - Vary1, 2)
+            );
+            Distancia = distancia.ToString("F2");
+
+
+        }
+
+
+    }
+}
+
